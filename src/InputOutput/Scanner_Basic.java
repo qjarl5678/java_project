3803af1088437ae0ae0d31537961351a78ade44f
qjarl5678 <qjarl5678@nate.com>
2024-05-08 21:47:10 +0900

    fix: 콘솔 에러 메시지 실습

--- src/InputOutput/Scanner_Basic.java
+++ src/InputOutput/Scanner_Basic.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Scanner_Basic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("일반 출력");
+        System.err.println("에러메시지 출력");
+
         while (true){
             System.out.println(sc.next());
         }
