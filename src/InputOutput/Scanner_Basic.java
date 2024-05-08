6400d0238b452cdf8d410a96dcf944f35cfe646c
qjarl5678 <qjarl5678@nate.com>
2024-05-08 21:39:53 +0900

    feat: console default

---
+++ src/InputOutput/Scanner_Basic.java
@@ -0,0 +1,13 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class Scanner_Basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println(sc.next());
+        }
+
+    }
+}
